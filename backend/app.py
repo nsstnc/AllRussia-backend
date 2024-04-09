d29511a7a97e8f42027ebb36e4258366dcc97f30
nsstnc <egorgolubev0484@gmail.com>
2024-04-09 14:02:12 +0300

    Simplified queries to DB Added new routes of news' tags

--- backend/app.py
+++ backend/app.py
@@ -201,25 +201,63 @@ def verifyExt(filename):
         return True
     return False
 
-@app.route("/data_news_sorted_by_date")
+# @app.route("/data_news_sorted_by_date")
+# def data_news_sorted_by_date():
+#     return json.dumps(database.get_news_sorted_by_date(), ensure_ascii=False)
+#
+# @app.route("/data_news_politics")
+# def data_news_politics():
+#     return json.dumps(database.get_news_politics(), ensure_ascii=False)
+#
+# @app.route("/data_news_economics")
+# def data_news_economics():
+#     return json.dumps(database.get_news_economics(), ensure_ascii=False)
+#
+# @app.route("/data_news_science_education")
+# def data_news_science_education():
+#     return json.dumps(database.get_news_science_education(), ensure_ascii=False)
+#
+# @app.route("/data_news_culture_history")
+# def data_news_culture_history():
+#     return json.dumps(database.get_news_culture_history(), ensure_ascii=False)
+
+
+@app.route("/data_all_news")
 def data_news_sorted_by_date():
-    return json.dumps(database.get_news_sorted_by_date(), ensure_ascii=False)
+    return json.dumps(database.get_news(sort_by_date_descending=True), ensure_ascii=False)
 
 @app.route("/data_news_politics")
 def data_news_politics():
-    return json.dumps(database.get_news_politics(), ensure_ascii=False)
+    return json.dumps(database.get_news(tag='Политика', sort_by_date_descending=True), ensure_ascii=False)
 
 @app.route("/data_news_economics")
 def data_news_economics():
-    return json.dumps(database.get_news_economics(), ensure_ascii=False)
+    return json.dumps(database.get_news(tag='Экономика', sort_by_date_descending=True), ensure_ascii=False)
 
 @app.route("/data_news_science_education")
 def data_news_science_education():
-    return json.dumps(database.get_news_science_education(), ensure_ascii=False)
+    return json.dumps(database.get_news(tag='Наука и образование', sort_by_date_descending=True), ensure_ascii=False)
 
 @app.route("/data_news_culture_history")
 def data_news_culture_history():
-    return json.dumps(database.get_news_culture_history(), ensure_ascii=False)
+    return json.dumps(database.get_news(tag='Культура и история', sort_by_date_descending=True), ensure_ascii=False)
+
+@app.route("/data_news_sport")
+def sport_data():
+    return json.dumps(database.get_news(tag='Спорт', sort_by_date_descending=True), ensure_ascii=False)
+
+@app.route("/data_news_tourism")
+def toursim_data():
+    return json.dumps(database.get_news(tag='Туризм', sort_by_date_descending=True), ensure_ascii=False)
+
+@app.route("/data_news_partners")
+def partners_data():
+    return json.dumps(database.get_news(tag='Партнеры', sort_by_date_descending=True), ensure_ascii=False)
+
+@app.route("/data_news_projects")
+def projects_data():
+    return json.dumps(database.get_news(tag='Проекты', sort_by_date_descending=True), ensure_ascii=False)
+
 
 
 print(__name__)
