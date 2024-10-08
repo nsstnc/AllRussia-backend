0f6709de1aa840e07957bc712905e49e4894b537
f4lz <krab336@mail.ru>
2024-10-09 00:53:02 +0300

    feat: Добавление `DropDown` меню

---
+++ frontend/src/components/Ui/UiDropDown.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { defineProps } from 'vue'
+
+defineProps({
+	options: Array
+})
+</script>
+
+<template>
+	<div class="drop-down" v-if="options.length">
+		<div v-for="option in options" :key="option">
+			<p class="drop-down__text">{{ option }}</p>
+		</div>
+	</div>
+</template>
+
+<style scoped>
+.drop-down {
+	display: flex;
+	align-items: center;
+	background-color: #222222;
+	flex-direction: column;
+	justify-content: center;
+	text-wrap: nowrap;
+	row-gap: 24px;
+	position: absolute;
+	padding: 8px 18px;
+}
+
+.drop-down__text {
+	margin: 0;
+	color: white;
+}
+</style>
